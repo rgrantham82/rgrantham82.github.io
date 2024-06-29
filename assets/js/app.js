3f075d70ee668f05bd36f7f1b8ce576fb5e05457
Robert Grantham <robertgrantham40@gmail.com>
2024-06-29 17:22:33 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   var burger = document.querySelector('.navbar-burger');
   var menu = document.querySelector('.navbar-menu');
-  
+
   burger.addEventListener('click', function() {
     burger.classList.toggle('is-active');
     menu.classList.toggle('is-active');
