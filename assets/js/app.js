1fddd4860af732be2334b2390f3121503feeed02
Robert Grantham <robertgrantham40@gmail.com>
2024-06-29 17:47:44 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  if (navbarBurgers.length > 0) {
+    navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        const target = el.dataset.target;
+        const targetElement = document.getElementById(target);
+        
+        el.classList.toggle('is-active');
+        targetElement.classList.toggle('is-active');
+      });
+    });
+  }
+});
+
 // Smooth scroll for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
